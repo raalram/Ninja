40afbbaa617b119052a59b3c02228229ba56768a
Lekar77 <lekar.studios@gmail.com>
2025-08-14 17:40:23 +0200

    Creando el crafting

--- Assets/Scripts/Tienda/ItemTienda.cs
+++ Assets/Scripts/Tienda/ItemTienda.cs
@@ -14,6 +14,15 @@ public class ItemTienda : MonoBehaviour
 
     public ItemVenta ItemCargado { get; private set; }
 
+    private int cantidad;
+    private int costoInicial;
+    private int costoActual;
+
+    private void Update()
+    {
+        cantidadPorComprar.text = cantidad.ToString();
+        itemCosto.text = costoActual.ToString();
+    }
 
     public void ConfigurarItemVenta(ItemVenta itemVenta)
     {
@@ -21,7 +30,39 @@ public class ItemTienda : MonoBehaviour
         itemIcono.sprite = itemVenta.Item.Icono;
         itemNombre.text = itemVenta.Item.Nombre;
         itemCosto.text = itemVenta.Costo.ToString();
+        cantidad = 1;
+        costoInicial = itemVenta.Costo;
+        costoActual = itemVenta.Costo;
 
+    }
+    public void ComprarItem()
+    {
+        if (MonedasManager.Instance.MonedasTotales >= costoActual)
+        {
+            Inventario.Instance.AñadirItem(ItemCargado.Item, cantidad);
+            MonedasManager.Instance.RemoverMonedas(costoActual);
+            cantidad = 1;
+            costoActual = costoInicial;
+        }
+    }
+    public void SumarItemPorComprar()
+    {
+        int costoDeCompra = costoInicial * (cantidad + 1);
+        if (MonedasManager.Instance.MonedasTotales >= costoDeCompra)
+        {
+            cantidad++;
+            costoActual = costoInicial * cantidad;
+        }
+    }
+    public void RestarItemPorComprar()
+    {
+        if (cantidad == 1)
+        {
+            return;
+        }
+
+        cantidad--;
+        costoActual = costoInicial * cantidad;
     }
 
 }
